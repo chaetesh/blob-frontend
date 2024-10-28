65bd7949d2ba892b6d905bd5ad8a3ef548f3de0d
chaetesh <MY_NAME@example.com>
2024-10-28 18:50:36 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FileUpload from './components/FileUpload'
+
+function App() {
+
+  return (
+    <>
+      <FileUpload></FileUpload>
+    </>
+  )
+}
+
+export default App
